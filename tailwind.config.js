3cbd6756085a30d22a2d67367cf3e3d61c7be2b0
ford2221 <fordstelle@gmail.com>
2021-06-29 15:16:59 -0400

    1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2978b5',
+        info: {
+          100: '#d8e3e7',
+          200: '#51c4d3',
+          300: '#126e82',
+          400: '#132c33'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
